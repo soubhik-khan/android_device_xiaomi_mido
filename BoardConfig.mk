c30377c8ae3f842978fcd10e8bc4c6736f740b07
soubhik-khan <soubhik.khan@gmail.com>
2017-06-11 18:21:13 +0530

    mido: nuke cmhw services

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,13 +108,6 @@ TARGET_TS_MAKEUP := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    device/xiaomi/mido/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
-
 # CNE / DPM
 BOARD_USES_QCNE := true
 
