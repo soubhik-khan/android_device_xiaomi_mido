17dc7acbaaab33079b14039007ba688f8cfe6719
soubhik-khan <soubhik.khan@gmail.com>
2017-06-18 20:50:27 +0530

    mido: build offline charger image

--- device.mk
+++ device.mk
@@ -257,3 +257,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Offmode charger
+PRODUCT_PACKAGES += \
+    charger_res_images
