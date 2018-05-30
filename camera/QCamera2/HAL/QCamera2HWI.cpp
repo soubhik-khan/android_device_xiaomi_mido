f319105b79e610b19d446f9cdce8ba91d7a3b636
Saritha Yamasani <syamas@codeaurora.org>
2018-05-30 17:48:50 +0530

    QCamera2:HAL1: startVsync during start/stop preview
    1)call startvsync in startPreview/stopPreview instead of boottime.
    
    2)init the vsync events in startVsync(1) and deinit in startVsync(0)
    to avoid the deadlocks at Surfaceflinger.
    Change-Id: I9b7e2e846d5215cd3433c55fe1c5e043946bcd0e
    (cherry-picked from commit bb72d713f5fbb25ce1ea6a6a540128de27e46a4c
    from platform/hardware/qcom/camera for msm-4.9)

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -2307,10 +2307,6 @@ int QCamera2HardwareInterface::closeCamera()
                 mCameraId);
     }
 
-#ifdef USE_DISPLAY_SERVICE
-        mCameraDisplay->startVsync(FALSE);
-#endif //Use_DISPLAY_SERVICE
-
     LOGI("[KPI Perf]: X PROFILE_CLOSE_CAMERA camera id %d, rc: %d",
          mCameraId, rc);
 
@@ -3521,6 +3517,12 @@ int QCamera2HardwareInterface::startPreview()
 
     m_perfLock.lock_acq();
 
+#ifdef USE_DISPLAY_SERVICE
+    if(!mCameraDisplay->startVsync(TRUE)) {
+        LOGE("Error: Cannot start vsync (still continue)");
+    }
+#endif //USE_DISPLAY_SERVICE
+
     updateThermalLevel((void *)&mThermalLevel);
 
     setDisplayFrameSkip();
@@ -3619,6 +3621,9 @@ int QCamera2HardwareInterface::stopPreview()
 #endif
     // delete all channels from preparePreview
     unpreparePreview();
+#ifdef USE_DISPLAY_SERVICE
+    mCameraDisplay->startVsync(FALSE);
+#endif //Use_DISPLAY_SERVICE
 
     m_perfLock.lock_rel();
 
